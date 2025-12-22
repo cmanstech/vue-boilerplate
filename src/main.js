2f19876c9e03ba419f3df7779148796450e96edf
pimpimys <pmpyrs_@hotmail.com>
2025-12-22 12:25:40 +0700

    init vue boilerplate

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import LenisVue from 'lenis/vue'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(LenisVue)
+
+app.mount('#app')
